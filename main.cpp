75355c9049b8f5b974be45c2ca999e467cf32377
nicoloroveri <116441124+nicoloroveri@users.noreply.github.com>
2022-11-14 17:38:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
-
 int main() {
+   float a = 0, b = 0, x = 0, y = 0;
+   cin >> a >> b >> x >> y;
+   float z = 0;
+   if((x<o) && (y>0)){
+      z = ((a*x)-(b*y));
+         cout << z <<endl;
+   }else if((x>=0) && (y<=0)){
+      z = ((a*x*x)-(b*y));
+         cout << z <<endl;
+   }else{
+      z = ((a*x)+(b*y*y));
+         cout << z <<endl;
+   }
    return 0;
 }
